2d024611389b8f30f59215cb2b1277c7e01923e4
baolibin <2578858653@qq.com>
2018-11-27 23:43:33 +0800

    mysql

---
+++ src/main/java/com/libin/mysql/java/JavaJdbc.java
@@ -0,0 +1,10 @@
+package com.libin.mysql.java;
+
+/**
+ * Copyright (c) 2018/11/27. xixi Inc. All Rights Reserved.
+ * Authors: libin <2578858653@qq.com>
+ * <p>
+ * Purpose :
+ */
+public class JavaJdbc {
+}
---
+++ src/main/java/com/libin/mysql/scala/ScalaJdbc.scala
@@ -0,0 +1,11 @@
+package com.libin.mysql.scala
+
+/**
+  * Copyright (c) 2018/11/27. xixi Inc. All Rights Reserved.
+  * Authors: libin <2578858653@qq.com>
+  * <p>
+  * Purpose :
+  */
+object ScalaJdbc {
+
+}
